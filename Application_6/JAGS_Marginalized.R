49072938fecdc6aca84ad428696671e46dace86c
sreynolds2 <sarhnc@umsystem.edu>
2024-08-27 11:17:41 -0400

    Update JAGS_Marginalized.R

--- Application_6/JAGS_Marginalized.R
+++ Application_6/JAGS_Marginalized.R
@@ -11,7 +11,6 @@
 #
 ###############################################################################
 library(R2jags)
-setwd("U:\\Desktop\\Fish_Git\\Marginalized")
 
 setwd(paste0(getwd(), '/Application_3'))
 
